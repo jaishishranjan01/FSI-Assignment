e1a7cff132db7ffcda455557ca9462f92b1432bb
rmishra <raghvendra.k.mishra@outlook.com>
2025-11-09 20:20:41 +0530

    Add readability comment for EquipmentServiceApplication  class

--- equipmentservice/src/main/java/org/bits/assignment/equipmentservice/EquipmentServiceApplication.java
+++ equipmentservice/src/main/java/org/bits/assignment/equipmentservice/EquipmentServiceApplication.java
@@ -3,6 +3,9 @@ package org.bits.assignment.equipmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Boot application entry point for the Equipment Service.
+ */
 @SpringBootApplication
 public class EquipmentServiceApplication {
     public static void main(String[] args) {
