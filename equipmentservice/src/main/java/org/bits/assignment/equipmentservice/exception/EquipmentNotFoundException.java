a0622eb638ef26d177ff571814dafb12c739743d
rmishra <raghvendra.k.mishra@outlook.com>
2025-11-09 20:20:16 +0530

    Add readability comment for EquipmentNotFoundException  class

--- equipmentservice/src/main/java/org/bits/assignment/equipmentservice/exception/EquipmentNotFoundException.java
+++ equipmentservice/src/main/java/org/bits/assignment/equipmentservice/exception/EquipmentNotFoundException.java
@@ -1,5 +1,8 @@
 package org.bits.assignment.equipmentservice.exception;
 
+/**
+ * Exception thrown when a requested Equipment entity cannot be found.
+ */
 public class EquipmentNotFoundException extends RuntimeException {
     public EquipmentNotFoundException(String msg){ super(msg); }
 }
