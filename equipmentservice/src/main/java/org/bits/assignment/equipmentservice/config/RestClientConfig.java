bfb4710e0e5655ef67ce938eff01605bfdaf905f
rmishra <raghvendra.k.mishra@outlook.com>
2025-11-09 20:18:22 +0530

    Add readability comment for Approval class

--- equipmentservice/src/main/java/org/bits/assignment/equipmentservice/config/RestClientConfig.java
+++ equipmentservice/src/main/java/org/bits/assignment/equipmentservice/config/RestClientConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration providing a RestTemplate bean for performing HTTP requests
+ * to external services.
+ */
 @Configuration
 public class RestClientConfig {
     @Bean
