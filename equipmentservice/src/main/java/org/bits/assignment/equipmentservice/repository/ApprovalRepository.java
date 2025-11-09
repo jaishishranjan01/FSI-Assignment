b1cc176b0edec1be4f4a10bb510693ef90fc0302
rmishra <raghvendra.k.mishra@outlook.com>
2025-11-09 20:18:46 +0530

    Add readability comment for ApprovalRepository class

--- equipmentservice/src/main/java/org/bits/assignment/equipmentservice/repository/ApprovalRepository.java
+++ equipmentservice/src/main/java/org/bits/assignment/equipmentservice/repository/ApprovalRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Repository for Approval entities.
+ * Provides CRUD operations and query methods to fetch approval records by status.
+ */
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
 
     List<Approval> findByStatus(Approval.Status status);
