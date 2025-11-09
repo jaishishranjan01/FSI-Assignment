3077b359d0e247f460ffaddac47056546009e119
rmishra <raghvendra.k.mishra@outlook.com>
2025-11-09 20:43:37 +0530

    Add readability comment for Role

--- user-service/src/main/java/org/bits/assignment/user_service/entity/Role.java
+++ user-service/src/main/java/org/bits/assignment/user_service/entity/Role.java
@@ -1,5 +1,12 @@
 package org.bits.assignment.user_service.entity;
 
+/**
+ * Application roles used to control access.
+ * Values:
+ * - STUDENT
+ * - ADMIN
+ * - STAFF
+ */
 public enum Role {
     STUDENT,
     ADMIN,
